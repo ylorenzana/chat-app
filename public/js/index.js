878036d56bf71d8db22852130127a95dd6574600
ylorenzana <yloren38@lakers.mercyhurst.edu>
2017-10-24 19:26:40 -0400

    Added newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+const socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+  
+  socket.emit('createMessage', {
+    from: 'Nik',
+    text: 'was good'
+  });
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('New message', message);
+});
--- server/server.js
+++ server/server.js
@@ -14,9 +14,20 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'example@gmail.com',
+    text: 'YOyoo',
+    createdAt: 'today'
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected from server');
-  })
+  });
 });
 
 server.listen(port, () => {
