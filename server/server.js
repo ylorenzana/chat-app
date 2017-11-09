1639a638413ffbf16e288c83cd9ac899c97919cf
ylorenzana <yloren38@lakers.mercyhurst.edu>
2017-11-08 20:19:47 -0500

    Add styles

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('From server');
+    callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
