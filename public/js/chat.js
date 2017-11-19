414a158b3ac11dc2251888ec61b35533e6391240
ylorenzana <yloren38@lakers.mercyhurst.edu>
2017-11-18 23:34:08 -0500

    Send/receive messages only to/from users in same chat room

--- public/js/chat.js
+++ public/js/chat.js
@@ -73,7 +73,6 @@ jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
   var messageTextBox = jQuery('[name=message]');
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextBox.val()
   }, function () {
     messageTextBox.val('');
